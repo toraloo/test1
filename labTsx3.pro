169d94abeb825f5537cfa4f60f0c7138c48ae79d
toraloo <toriejiang1991@sina.cn>
2016-07-28 11:23:42 +0800

    1.0.0.2

--- labTsx3.pro
+++ labTsx3.pro
@@ -13,8 +13,10 @@ TEMPLATE = app
 
 
 SOURCES += main.cpp\
-        mainwindow.cpp
+        mainwindow.cpp \
+    mywidget.cpp
 
-HEADERS  += mainwindow.h
+HEADERS  += mainwindow.h \
+    mywidget.h
 
 FORMS    += mainwindow.ui
---
+++ mywidget.h
@@ -0,0 +1,17 @@
+#ifndef MYWIDGET_H
+#define MYWIDGET_H
+
+#include <QWidget>
+
+class myWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit myWidget(QWidget *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // MYWIDGET_H
